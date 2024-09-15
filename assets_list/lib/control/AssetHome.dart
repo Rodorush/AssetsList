1b5621903a8e88927a519721bf5930d405e73c3e
Rodolfo Pereira de Andrade <rodolfop.andrade@gmail.com>
2024-09-15 10:53:52 -0300

    Evento de clique no item com snackbar
    • Criado evento de clique na lista com snackbar

--- assets_list/lib/control/AssetHome.dart
+++ assets_list/lib/control/AssetHome.dart
@@ -11,13 +11,11 @@ class AssetHome extends StatefulWidget {
 }
 
 class _AssetHomeState extends State<AssetHome> {
-
-  List<WordPair> _assetList = <WordPair>[];
-  TextStyle _biggerFont = const TextStyle(fontSize: 18);
+  final List<WordPair> _assetList = <WordPair>[];
+  final TextStyle _biggerFont = const TextStyle(fontSize: 18);
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
@@ -29,28 +27,43 @@ class _AssetHomeState extends State<AssetHome> {
 
   Widget _buildAssetList() {
     return ListView.builder(
-        padding: EdgeInsets.all(16),
-
-        itemBuilder: (BuildContext _context, int i) {
-          if(i.isOdd) { return Divider(); }
+        padding: const EdgeInsets.all(16),
+        itemBuilder: (BuildContext context, int i) {
+          if (i.isOdd) {
+            return Divider();
+          }
           int index = i ~/ 2;
 
-          if(index >= _assetList.length) {
+          if (index >= _assetList.length) {
             _assetList.addAll(generateWordPairs().take(10));
           }
 
-          return _assetRow(_assetList[index]);
-        }
-    );
+          return _buildRow(_assetList[index]);
+        });
   }
 
-  Widget _assetRow(WordPair asset) {
-
+  Widget _buildRow(WordPair asset) {
     return ListTile(
       title: Text(
         asset.asPascalCase,
         style: _biggerFont,
       ),
+      onTap: () {
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Center(
+              child: Text(
+            asset.asPascalCase,
+            style: const TextStyle(fontSize: 20),
+          )),
+          duration: const Duration(milliseconds: 1000),
+          width: 180.0,
+          padding: const EdgeInsets.all(16.0),
+          behavior: SnackBarBehavior.floating,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+        ));
+      },
     );
   }
 }
